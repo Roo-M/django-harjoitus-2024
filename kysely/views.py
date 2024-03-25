697c2497389235ca59543754e9db74c7967718a4
Roo-M <51594@edu.raseko.fi>
2024-03-25 20:32:30 +0200

    sdf

--- kysely/views.py
+++ kysely/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def indeksi(request):
+    return HttpResponse("AAAAAAA! Olet kysely-appin index-sivulla.")
+
