3f861f6b78947997405824378c07c6d385bd458d
Roo-M <51594@edu.raseko.fi>
2024-03-27 20:20:27 +0200

    lisää muutama näkymä

--- kysely/views.py
+++ kysely/views.py
@@ -5,3 +5,14 @@ from django.http import HttpResponse
 def indeksi(request):
     return HttpResponse("AAAAAAA! Olet kysely-appin index-sivulla.")
 
+
+def näytä(request, question_id):
+    return HttpResponse(f"Katsot juuri kysymystä {question_id}.")
+
+
+def tulokset(request, question_id):
+    return HttpResponse(f"Katsot kysymyksen {question_id} tuloksia.")
+
+
+def äänestä(request, question_id):
+    return HttpResponse(f"Olet äänestämässä kysmykseen {question_id}.")
